e877d29e8ce037ac2310a0d993527342eab99a7c
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-13 11:31:32 +0600

    feat: Enhance profile component with modal updates and address card enhancements
    This commit introduces several enhancements to the profile component:
    
    - Added a close button with an SVG icon to the Modal component.
    - Enhanced the AddressCard component with styling and implemented a modal for address editing.
    - Removed the PersonalInfoCard component and its corresponding import.
    - Updated the ProfileCard component to include the new modal for personal info editing.
    - Removed the wrapping container in UserProfile.vue to ensure the content is shown appropriately.

--- src/components/profile/ProfileCard.vue
+++ src/components/profile/ProfileCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div
-            class="p-5 mb-6 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6"
+            class="rounded-2xl border border-gray-200 bg-white p-5 mb-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6"
         >
             <div
                 class="flex flex-col gap-5 xl:flex-row xl:items-center xl:justify-between"
@@ -140,27 +140,6 @@
 
         <Modal v-if="isProfileInfoModal" @close="isProfileInfoModal = false">
             <template #header>
-                <!-- close btn -->
-                <button
-                    @click="isProfileAddressModal = false"
-                    class="transition-color absolute right-5 top-5 z-999 flex h-11 w-11 items-center justify-center rounded-full bg-gray-100 text-gray-400 hover:bg-gray-200 hover:text-gray-600 dark:bg-gray-700 dark:bg-white/[0.05] dark:text-gray-400 dark:hover:bg-white/[0.07] dark:hover:text-gray-300"
-                >
-                    <svg
-                        class="fill-current"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M6.04289 16.5418C5.65237 16.9323 5.65237 17.5655 6.04289 17.956C6.43342 18.3465 7.06658 18.3465 7.45711 17.956L11.9987 13.4144L16.5408 17.9565C16.9313 18.347 17.5645 18.347 17.955 17.9565C18.3455 17.566 18.3455 16.9328 17.955 16.5423L13.4129 12.0002L17.955 7.45808C18.3455 7.06756 18.3455 6.43439 17.955 6.04387C17.5645 5.65335 16.9313 5.65335 16.5408 6.04387L11.9987 10.586L7.45711 6.04439C7.06658 5.65386 6.43342 5.65386 6.04289 6.04439C5.65237 6.43491 5.65237 7.06808 6.04289 7.4586L10.5845 12.0002L6.04289 16.5418Z"
-                            fill=""
-                        />
-                    </svg>
-                </button>
                 <div class="px-2 pr-14">
                     <h4
                         class="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90"
